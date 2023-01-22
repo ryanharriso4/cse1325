c1f01e158368d242dd376e467015e714943b9f06
RyanHarris04 <ryanthomasharris@gmail.com>
2023-01-21 20:32:04 -0600

    P01 bonus

---
+++ P01/bonus/Hello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; 
+
+public class  Hello{
+	public static void main(String [] args)
+	{
+		Scanner input = new Scanner(System.in); 
+		System.out.print("What is your name? "); 
+		String name = input.nextLine(); 
+		System.out.println("Hello, " + name + "!"); 
+	}
+}
