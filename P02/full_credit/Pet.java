c566f80001bf77eb271d493ccc6450b86b117b72
RyanHarris04 <ryanthomasharris@gmail.com>
2023-01-25 10:25:50 -0600

    Framework on Pet done

---
+++ P02/full_credit/Pet.java
@@ -0,0 +1,19 @@
+public class pet{
+
+
+	private String name; 
+	private double age; 
+	private Type type; 
+
+	public pet(String name, double age, Type type)
+	{
+		this.name = name; 
+		this.age = age; 
+		this.type = type; 
+	}
+
+	public String toString()
+	{
+		
+	}
+}
