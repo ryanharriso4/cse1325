b910984ac8f00ee6433e960975e350e4aebc9ceb
RyanHarris04 <ryanthomasharris@gmail.com>
2023-02-01 12:39:57 -0600

    Added the build.xml file and wrote some code for the Quiz and Question classes

--- P03/full_credit/Question.java
+++ P03/full_credit/Question.java
@@ -2,14 +2,18 @@ public class Question{
 	private String question; 
 	private String [] answers; 
 	private int rightAnswer;
-	private int nextQuestionNumber = 1; 
-	private final int questionNumber; 
+	private static int nextQuestionNumber = 1; 
+	private final int questionNumber;  
 
 	public Question(String question, String [] answers, int rightAnswer)
 	{
 		this.question = question;
 		this.answers = answers; 
-		this.rightAnswer = rightAnswer; 
+		questionNumber = nextQuestionNumber++; 
+		if((rightAnswer < 1) || (rightAnswer > answers.length))
+		{
+			throw new IllegalArgumentException("The Number of correct answers chosen is wrong. Correct answers must be in between 1 and " + answers.length + "(The number of answers entered)"); 
+		}
 	}
 
 	public boolean checkAnswer(int answer)
@@ -20,6 +24,11 @@ public class Question{
 	@Override
 	public String toString()
 	{
-
+		String output = questionNumber + ". " + question + "\n\t"; 
+		for(int i = 0; i < answers.length; i++)
+		{
+			output += i + 1 ")  " + answers[i] + "\n\t";
+		}
+		return output; 
 	}
 }
--- P03/full_credit/Quiz.java
+++ P03/full_credit/Quiz.java
@@ -4,7 +4,7 @@ public class Quiz{
 
 	public Quiz()
 	{
-
+		loadQuiz(); 
 	}
 
 	public double takeQuiz()
@@ -14,6 +14,9 @@ public class Quiz{
 
 	private void loadQuiz()
 	{
-		
+		String [] Q1Answers = {"-2","-1","100", "2"}; 
+		String [] Q2Answers = {"5", "100", "-2", "2"}; 
+		Question Q1 = new Question("What is 1 + 1?", Q1Answers, 4);
+		Question Q2 = new Question("What is 5 x 20?", Q2Answers, 2); 
 	}
 }
