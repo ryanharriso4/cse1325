9c52fa6ef915b3acf906392a17d01a1f4988ab49
RyanHarris04 <ryanthomasharris@gmail.com>
2023-02-07 15:37:58 -0600

    Added layout for product.java

--- P04/full_credit/Product.java
+++ P04/full_credit/Product.java
@@ -1,3 +1,21 @@
 public abstract class Product{
-	
+	protected double cost; 
+	protected String name; 
+
+	public Product(String name, double cost)
+	{
+		this.name = name; 
+		this.cost = cost; 
+	}
+
+	public abstract double price()
+	{
+
+	}
+
+	@Override
+	public String toString()
+	{
+
+	}
 }
