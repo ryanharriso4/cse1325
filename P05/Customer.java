1f0af47f500ed356a7e7deb368b9ea1a22bc9cfd
RyanHarris04 <ryanthomasharris@gmail.com>
2023-02-23 16:06:32 -0600

    Adding build.xml. Finished testing the constructor and toString for customer.

--- P05/Customer.java
+++ P05/Customer.java
@@ -5,9 +5,9 @@ public class Customer{
 	public Customer(String name, String email)
 	{
 		this.name = name; 
-		if(!(email.contains("@")) && !(email.contains(".")) && (email.indexOf(".") < email.indexOf("@")))
+		if(!(email.contains("@")) || !(email.contains(".")) || (email.indexOf(".") < email.indexOf("@")))
 		{
-			throw new IllegalArgumentException("The email for user " + name " is invalid.\n"); 
+			throw new IllegalArgumentException("The email for user " +  name  +  " is invalid.\n"); 
 		}
 		this.email = email; 
 	}
